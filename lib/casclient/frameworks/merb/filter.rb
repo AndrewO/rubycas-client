d419c9b2b9d4d487cd56ac1136a23796cd06feac
Andrew O'Brien <aobrien@Ziggy.local>
2008-03-04 23:29:05 -0500

    Merb filter now uses previous tickets

--- lib/casclient/frameworks/merb/filter.rb
+++ lib/casclient/frameworks/merb/filter.rb
@@ -11,8 +11,7 @@ module CASClient
           
           cas_login_url = client.add_service_to_login_url(read_service_url(self))
           
-          last_service_ticket = controller.session[:cas_last_valid_ticket]
-          
+          last_service_ticket = session[:cas_last_valid_ticket]
           if (service_ticket && 
             last_service_ticket && 
             last_service_ticket.ticket == service_ticket.ticket && 
@@ -22,10 +21,21 @@ module CASClient
             # happens to be in the URL.
             log.warn("Reusing previously validated ticket since the new ticket and service are the same.")
             service_ticket = last_service_ticket
+          elsif last_service_ticket &&
+              !config[:authenticate_on_every_request] && 
+              session[client.username_session_key]
+            # Re-use the previous ticket if the user already has a local CAS session (i.e. if they were already
+            # previously authenticated for this service). This is to prevent redirection to the CAS server on every
+            # request.
+            # This behaviour can be disabled (so that every request is routed through the CAS server) by setting
+            # the :authenticate_on_every_request config option to false. 
+            log.debug "Existing local CAS session detected for #{session[client.username_session_key].inspect}. "+
+              "Previous ticket #{last_service_ticket.ticket.inspect} will be re-used."
+            service_ticket = last_service_ticket
           end
           
           if service_ticket
-            client.validate_service_ticket(service_ticket)
+            client.validate_service_ticket(service_ticket) unless service_ticket.has_been_validated?
             validation_response = service_ticket.response
             
             if service_ticket.is_valid?
